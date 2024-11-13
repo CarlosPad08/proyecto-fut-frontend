9b8dbcb2b9c1ecbc0162e832ee9d04b729564125
Padilla0206 <carlosfpm123@gmail.com>
2024-11-13 09:23:43 -0500

    Traspaso a React

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='body'>
+          {/* Barra de navegación */}
+        <nav className="navbar">
+            {/* Botón de Login que activa el checkbox */}
+            <label htmlFor="toggle-login" className="nav-button">Login</label>
+            {/* Botón de Register (sin acción asociada por ahora) */}
+            <button className="nav-button" /*onClick="location.href='#'"*/>Register</button>
+        </nav>
+
+        {/* Checkbox oculto que se usa para mostrar el formulario de login */}
+        <input type="radio" id="toggle-login" />
+        
+        {/* Contenedor que contiene el formulario de login */}
+        <div className="contenedor">
+            <h2>Iniciar sesión</h2>
+            <form id="forma" name="forma" method="post">
+                <div className="elemento">
+                    <label htmlFor="usuario">Usuario</label>
+                    <input type="text" id="usuario" name="usuario" /*required="true"*//>
+                </div>
+                <div className="elemento">
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" id="password" name="password" /*required="true"*//>
+                </div>
+                <div className="elemento">
+                    <input type="submit" value="Ingresar"/>
+                </div>
+            </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
